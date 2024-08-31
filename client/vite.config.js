7d225a4c586e7d3ded533f85605bc0061ddb6b43
Ajay Durgesam <a.durgesam@iitg.ac.in>
2024-08-31 17:13:57 +0530

    url modified

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://taskmanager-ylhk.onrender.com",
+        target: "http://localhost:8800",
         changeOrigin: true,
       },
     },
